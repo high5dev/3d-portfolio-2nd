420aad91630d19361c0c9e0eaf78c575cc390e14
high5dev <high5dev621@hotmail.com>
2024-10-08 14:22:05 -0400

    floor effect changed

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,4 @@
-import { Environment, OrbitControls, Sky } from "@react-three/drei";
+import { ContactShadows, Environment, OrbitControls, Sky } from "@react-three/drei";
 import { Avatar } from "./Avatar";
 import { useControls } from "leva";
 
@@ -16,6 +16,7 @@ export const Experience = () => {
       <Sky />
       <Environment preset="sunset" />
       <group position-y={-1}>
+        <ContactShadows opacity={0.42} scale={10} blur={1} far={10} resolution={256} color="#000000" />
         <Avatar animation={animation} />
         {
           animation === "Typing" && 
@@ -26,7 +27,8 @@ export const Experience = () => {
         }
         <mesh
           scale={5}
-          rotation-x={Math.PI * 0.5}>
+          rotation-x={Math.PI * 0.5}
+          position-y={-0.001}>
           <planeGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
